2283048eed6418aa6ca6ded3be7f87115f975b62
josephmfaulkner <josephmfaulkner@gmail.com>
2022-10-22 18:46:03 -0700

    initialize SpringBoot webapp

---
+++ api/src/test/java/com/josephmfaulkner/simplenetflixapi/SimplenetflixapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephmfaulkner.simplenetflixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenetflixapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
